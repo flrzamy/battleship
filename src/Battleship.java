06863ac9e5c815e333dd40ddee40c1e0e589001a
AmyFangLittlescribe <amy.fang@littlescribe.com>
2020-09-08 21:16:42 +1000

    start placing ships

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Battleship {
+
+    final String[] ships = {"Carrier", "Battleship", "Destroyer", "Submarine", "PatrolBoat"};
+
 
     public void start() {
         Board myBoard = new Board();
@@ -10,22 +15,30 @@ public class Battleship {
         System.out.println("Please start the game by placing your battleships on your field.\n");
         //Start placing ships
         boolean placed = false;
-        while (!placed) {
-            System.out.println("Now place your Carrier, which is of size 5.");
-            System.out.println("Would you like to place your Carrier horizontally(H) or vertically(V)?");
-            System.out.print("[H/V]");
-            Scanner scanner = new Scanner(System.in);
-            String direction = scanner.next();
-            System.out.println("\nOn Which square would you like to place the head of your Carrier?");
-            myBoard.printBoard();
-            System.out.print("[A1-H8]:");
-            String index = scanner.next();
-            if (!myBoard.placeShip('C',index,direction)) {
-                System.out.println("Invalid position. Please try again.\n");
-            } else {
-                placed = true;
+        int numPlaced = 0;
+        String ship = ships[0];
+        int size = 5;
+        while (numPlaced<5) {
+            while (!placed) {
+                System.out.println("Now place your "+ship+", which is of size "+size+".");
+                System.out.println("Would you like to place your "+ship+" horizontally(H) or vertically(V)?");
+                System.out.print("[H/V]");
+                Scanner scanner = new Scanner(System.in);
+                String direction = scanner.next();
+                System.out.println("\nOn Which square would you like to place the head of your "+ship+"?");
+                myBoard.printBoard();
+                System.out.print("[A1-H8]:");
+                String index = scanner.next();
+                if (!myBoard.placeShip(ship.charAt(0),index,direction)) {
+                    System.out.println("Invalid position. Please try again.\n");
+                } else {
+                    placed = true;
+                    numPlaced++;
+                    ship = ships[numPlaced];
+                }
             }
         }
+
 
     }
 }
--- src/Board.java
+++ src/Board.java
@@ -10,8 +10,9 @@ public class Board {
     }
 
     void printBoard() {
-        System.out.println(" A B C D E F G H");
+        System.out.println("   A B C D E F G H");
         for (int i=0; i<8; i++) {
+            System.out.print(i+1+" ");
             for (int j=0; j<8; j++) {
                 System.out.print("|"+board[i][j]);
             }
